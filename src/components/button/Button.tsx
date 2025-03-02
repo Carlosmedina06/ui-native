09c032785c64d0c7c20685a88e8f6b09675dc9d3
carlosmedina06 <slaawmedina14@gmail.com>
2025-03-02 15:46:17 -0300

    agregar variantes y tamaños al componente Button

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -9,16 +9,39 @@ interface ButtonProps {
   onPress: () => void;
   className?: string;
   style?: StyleProp<ViewStyle>;
+  variant?: 'default' | 'destructive' | 'outline' | 'secondary' | 'link';
+  size?: 'default' | 'sm' | 'lg';
 }
+
+const variants = {
+  default: 'bg-primary text-primary-foreground shadow-xs',
+  destructive: 'bg-destructive text-white shadow-xs',
+  outline: 'border border-input bg-background shadow-xs',
+  secondary: 'bg-secondary text-secondary-foreground shadow-xs',
+  link: 'text-primary underline-offset-4',
+};
+
+const sizes = {
+  default: 'h-9 px-4 py-2',
+  sm: 'h-8 rounded-md gap-1.5 px-3',
+  lg: 'h-10 rounded-md px-6',
+};
 
 const StyledButton = cssInterop(TouchableOpacity, {
   className: 'style',
 });
 
-const Button: React.FC<ButtonProps> = ({ title, onPress, className, style }) => {
+const Button: React.FC<ButtonProps> = ({
+  title,
+  onPress,
+  className,
+  style,
+  variant = 'default',
+  size = 'default',
+}) => {
   return (
     <StyledButton
-      className={cn('rounded-md bg-blue-500  px-5 py-3  text-white ', className)}
+      className={cn('', variants[variant], sizes[size], className)}
       style={style}
       onPress={onPress}
     >
